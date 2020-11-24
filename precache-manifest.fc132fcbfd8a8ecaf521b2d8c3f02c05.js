5f945f6a5fff44760b57107f699637f63db43406
DESKTOP-U2BJ7C7\Mudher Domili <mudher.d@gmail.com>
2020-11-24 16:18:57 +0800

    no message

---
+++ precache-manifest.fc132fcbfd8a8ecaf521b2d8c3f02c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a94abb31dfb2863ceb53",
+    "url": "/static/css/main.1de05c0f.chunk.css"
+  },
+  {
+    "revision": "a94abb31dfb2863ceb53",
+    "url": "/static/js/main.c9c8b4f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "96df0547005d473a18618aefbceddf18",
+    "url": "/index.html"
+  }
+];
